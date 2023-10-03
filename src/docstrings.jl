e46501a8261f60cae5e1794576446cd2e71c1316
drizk1 <rizkytennis@gmail.com>
2023-10-03 16:04:52 -0400

    added ; to df in missing_if docstring

--- src/docstrings.jl
+++ src/docstrings.jl
@@ -2454,7 +2454,7 @@ Replace a specific `value` with `missing` in `x`.
 
 ## Examples
 ```jldoctest
-julia> df = DataFrame(a = [1, missing, 3, 4], b = ["apple", "apple", "banana", "cherry"])
+julia> df = DataFrame(a = [1, missing, 3, 4], b = ["apple", "apple", "banana", "cherry"]);
 
 julia> @chain df begin
        @mutate(a = missing_if(a, 4), b = missing_if(b, "apple"))
