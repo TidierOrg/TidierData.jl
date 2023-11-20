3111ca1ea3dbefbbcb4556fa1831a464301e3e85
Karandeep Singh <kdpsingh@umich.edu>
2023-11-20 18:43:07 -0500

    Adds `@anti_join()` and `@semi_join()`, bump version to 0.13.2

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ TidierData.jl currently supports the following top-level macros:
 - `@arrange()`
 - `@pull()`
 - `@count()` and `@tally()`
-- `@left_join()`, `@right_join()`, `@inner_join()`, and `@full_join()`
+- `@left_join()`, `@right_join()`, `@inner_join()`, `@full_join()`, `@anti_join()`, and `@semi_join()`
 - `@bind_rows()` and `@bind_cols()`
 - `@pivot_wider()` and `@pivot_longer()`
 - `@separate()` and `@unite()`
--- src/joins.jl
+++ src/joins.jl
@@ -105,3 +105,57 @@ macro full_join(df1, df2)
   end
   return df_expr
 end
+
+"""
+$docstring_anti_join
+"""
+macro anti_join(df1, df2, by)
+  by = parse_join_by(by)
+
+  df_expr = quote
+    antijoin(DataFrame($(esc(df1))), DataFrame($(esc(df2))); on = $by)
+  end
+  if code[]
+    @info MacroTools.prettify(df_expr)
+  end
+  return df_expr
+end
+
+macro anti_join(df1, df2)
+  by = :(intersect(names(DataFrame($(esc(df1)))), names(DataFrame($(esc(df2))))))
+
+  df_expr = quote
+    antijoin(DataFrame($(esc(df1))), DataFrame($(esc(df2))); on = $by)
+  end
+  if code[]
+    @info MacroTools.prettify(df_expr)
+  end
+  return df_expr
+end
+
+"""
+$docstring_semi_join
+"""
+macro semi_join(df1, df2, by)
+  by = parse_join_by(by)
+
+  df_expr = quote
+    semijoin(DataFrame($(esc(df1))), DataFrame($(esc(df2))); on = $by)
+  end
+  if code[]
+    @info MacroTools.prettify(df_expr)
+  end
+  return df_expr
+end
+
+macro semi_join(df1, df2)
+  by = :(intersect(names(DataFrame($(esc(df1)))), names(DataFrame($(esc(df2))))))
+
+  df_expr = quote
+    semijoin(DataFrame($(esc(df1))), DataFrame($(esc(df2))); on = $by)
+  end
+  if code[]
+    @info MacroTools.prettify(df_expr)
+  end
+  return df_expr
+end
