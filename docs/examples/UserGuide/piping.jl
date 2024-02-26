bb089ddb3f3b5262b48bc837afd926768c807df5
Karandeep Singh <karandeep@gmail.com>
2024-02-26 10:29:00 -0500

    Converted commented code into example block.

--- docs/examples/UserGuide/piping.jl
+++ docs/examples/UserGuide/piping.jl
@@ -118,6 +118,7 @@ end
 
 # What's nice about this approach is that if you want to remove some criteria, you can easily comment out the relevant parts. For example, if you're willing to consider older movies, just comment out the `Year >= 2000`.
 
+```@example
 @chain movies begin
   @filter begin
     # Year >= 2000
@@ -125,6 +126,7 @@ end
   end
   @slice 1:5
 end
+```
 
 # ## Which approach to use?
 
