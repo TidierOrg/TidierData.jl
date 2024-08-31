3c274315110daacb30cfc0aa913b17e3ec689971
Michael Johnson <mistermichaellll@gmail.com>
2024-08-31 13:33:19 -0400

    remove sequential tests
    This isn't one-to-one with dplyr because of how dataframes works under the hood

--- test/test_mutate.jl
+++ test/test_mutate.jl
@@ -12,50 +12,6 @@
         @test isequal(@mutate(gf, []), gf)
     end
 
-    @testset "mutations applied progressively" begin
-        df = DataFrame(x = 1)
-        @test isequal(
-            (@mutate df begin
-                    y = x + 1
-                    z = y + 1
-                end
-            ),
-            DataFrame(x = 1, y = 2, z = 3)
-        )
-
-        @test isequal(
-            (@mutate df begin
-                    x = x + 1
-                    x = x + 1
-                end
-            ),
-            DataFrame(x = 3)
-        )
-
-        @test isequal(
-            (@mutate df begin
-                    x = 2
-                    y = x
-                end
-            ),
-            DataFrame(x = 2, y = 2)
-        )
-
-        df = DataFrame(x = 1, y = 2)
-        @test isequal(
-            (@mutate df begin
-                x2 = x
-                x3 = x2 + 1
-            end
-            ),
-            (@mutate df begin
-                x2 = x + 0
-                x3 = x2 + 1
-            end
-            )
-        )
-    end
-
     @testset "length-1 vectors are recycled" begin
         df = DataFrame(x = 1:4)
         @test isequal(@mutate(df, y = 1)[!, :y], fill(1, 4))
