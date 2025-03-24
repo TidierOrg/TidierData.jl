3b1c3663e3c7f449769f268ce3afa60e8f95b3b2
Karandeep Singh <karandeep@gmail.com>
2025-03-24 13:00:57 +0530

    exclude log.jl from code coverage for now

--- src/log.jl
+++ src/log.jl
@@ -1,3 +1,5 @@
+# COV_EXCL_START
+
 function generate_log(df_copy, df_output, name, modes)
     message = ""
     for mode in modes
@@ -207,3 +209,5 @@ function log_separate_changes(df_before::DataFrame, df_after::DataFrame, into)
     end
     return msg
 end
+
+# COV_EXCL_STOP
